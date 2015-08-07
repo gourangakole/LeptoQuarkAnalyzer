be4832a359c1e51915f8643ddb8d3b035c6bdb2a
Bibhuprasad Mahakud <bibhu.phy@gmail.com>
2015-08-07 17:44:02 +0200

    process.TreeMaker2. added to few lines

--- test/Config_analyzer.py
+++ test/Config_analyzer.py
@@ -21,7 +21,7 @@ from TreeMaker.TreeMaker.makeTreeFromMiniAOD_cff import makeTreeFromMiniAOD
 makeTreeFromMiniAOD(process,
                     outfile="ReducedSelection",
                     reportfreq=50,
-                    dataset="file:/afs/cern.ch/user/b/bmahakud/DYJetsToLL_M-50_HT-600toInf_Tune4C_13TeV-madgraph-tauola_MINIAODSIM_PU20bx25_PHYS14_25_V1-v1.root",
+                    dataset="file:/afs/cern.ch/user/b/bmahakud/public/DYJetsToLL_M-50_HT-600toInf_Tune4C_13TeV-madgraph-tauola_MINIAODSIM_PU20bx25_PHYS14_25_V1-v1.root",
                     globaltag="PHYS14_25_V2::All",
                     lostlepton=False,
                     numevents=500,
@@ -44,36 +44,37 @@ process.TreeMaker2.VectorBool           = cms.vstring()
 
 ###  - - - - - - - - Lepton stuff - - - - - - - 
 
-process.LeptonsNewTag.minElecPt        = cms.double(35),
-process.LeptonsNewTag.maxElecEta       = cms.double(2.5),
-process.LeptonsNewTag.minMuPt          = cms.double(10),
-process.LeptonsNewTag.maxMuEta         = cms.double(2.4),
-process.LeptonsNewTag.UseMiniIsolation = cms.bool(True),
-process.LeptonsNewTag.muIsoValue       = cms.double(0.2),
-process.LeptonsNewTag.elecIsoValue     = cms.double(0.1), # only has an effect when used with miniIsolation
+process.LeptonsNewTag.minElecPt        = cms.double(35)
+process.LeptonsNewTag.maxElecEta       = cms.double(2.5)
+process.LeptonsNewTag.minMuPt          = cms.double(10)
+process.LeptonsNewTag.maxMuEta         = cms.double(2.4)
+process.LeptonsNewTag.UseMiniIsolation = cms.bool(True)
+process.LeptonsNewTag.muIsoValue       = cms.double(0.2)
+process.LeptonsNewTag.elecIsoValue     = cms.double(0.1) # only has an effect when used with miniIsolation
 
-VarsInt.extend(['LeptonsNew(Leptons)'])
-VectorRecoCand.extend(['LeptonsNew:IdIsoMuon(Muons)','LeptonsNew:IdIsoElectron(Electrons)'])
-VectorInt.extend(['LeptonsNew:MuonCharge(MuonCharge)','LeptonsNew:ElectronCharge(ElectronCharge)'])
+process.TreeMaker2.VarsInt.extend(['LeptonsNew(Leptons)'])
+process.TreeMaker2.VectorRecoCand.extend(['LeptonsNew:IdIsoMuon(Muons)','LeptonsNew:IdIsoElectron(Electrons)'])
+process.TreeMaker2.VectorInt.extend(['LeptonsNew:MuonCharge(MuonCharge)','LeptonsNew:ElectronCharge(ElectronCharge)'])
 
 ###  - - - - - - - - Jet stuff - - - - - - - 
 
-process.GoodJets.maxJetEta                 = cms.double(2.4),
-process.GoodJets.maxMuFraction             = cms.double(2),
-process.GoodJets.minNConstituents          = cms.double(2),
-process.GoodJets.maxNeutralFraction        = cms.double(0.90),
-process.GoodJets.maxPhotonFraction         = cms.double(0.95),
-process.GoodJets.minChargedMultiplicity    = cms.double(0),
-process.GoodJets.minChargedFraction        = cms.double(0),
-process.GoodJets.maxChargedEMFraction      = cms.double(0.99),
-process.GoodJets.jetPtFilter               = cms.double(45),
-process.GoodJets.ExcludeLepIsoTrackPhotons = cms.bool(True),
-process.GoodJets.JetConeSize               = cms.double(0.3),
-process.GoodJets.MuonTag                   = cms.InputTag('LeptonsNew:IdIsoMuon'),
-process.GoodJets.ElecTag                   = cms.InputTag('LeptonsNew:IdIsoElectron'),
+process.GoodJets.maxJetEta                 = cms.double(2.4)
+process.GoodJets.maxMuFraction             = cms.double(2)
+process.GoodJets.minNConstituents          = cms.double(2)
+process.GoodJets.maxNeutralFraction        = cms.double(0.90)
+process.GoodJets.maxPhotonFraction         = cms.double(0.95)
+process.GoodJets.minChargedMultiplicity    = cms.double(0)
+process.GoodJets.minChargedFraction        = cms.double(0)
+process.GoodJets.maxChargedEMFraction      = cms.double(0.99)
+process.GoodJets.jetPtFilter               = cms.double(45)
+process.GoodJets.ExcludeLepIsoTrackPhotons = cms.bool(True)
+process.GoodJets.JetConeSize               = cms.double(0.3)
+process.GoodJets.MuonTag                   = cms.InputTag('LeptonsNew:IdIsoMuon')
+process.GoodJets.ElecTag                   = cms.InputTag('LeptonsNew:IdIsoElectron')
 
-VarsBool.extend(['GoodJets(JetID)'])
+process.TreeMaker2.VarsBool.extend(['GoodJets(JetID)'])
 process.TreeMaker2.VectorRecoCand.extend(['GoodJets(Jets)'])
+
 process.TreeMaker2.VectorDouble.extend(['JetsProperties:bDiscriminatorUser(Jets_bDiscriminatorCSV)',
                                             'JetsProperties:bDiscriminatorMVA(Jets_bDiscriminatorMVA)',
                                             'JetsProperties:chargedEmEnergyFraction(Jets_chargedEmEnergyFraction)',
@@ -90,18 +91,18 @@ process.TreeMaker2.VectorInt.extend(['JetsProperties:chargedHadronMultiplicity(J
                                          'JetsProperties:flavor(Jets_flavor)'])
 
 ###  - - - - - - - - Event Filter stuff - - - - - - - 
-VarsInt.extend(['METFilters'])
-VarsInt.extend(['CSCTightHaloFilter'])
-VarsInt.extend(['HBHENoiseFilter'])
-VarsInt.extend(['EcalDeadCellTriggerPrimitiveFilter'])
+process.TreeMaker2.VarsInt.extend(['METFilters'])
+process.TreeMaker2.VarsInt.extend(['CSCTightHaloFilter'])
+process.TreeMaker2.VarsInt.extend(['HBHENoiseFilter'])
+process.TreeMaker2.VarsInt.extend(['EcalDeadCellTriggerPrimitiveFilter'])
 
 ###  - - - - - - - - Event Filter stuff - - - - - - - 
 
 process.TriggerProducer.triggerNameList = cms.vstring( 'HLT_Ele15_IsoVVVL_PFHT350_PFMET70_v' )
 
-VectorBool.extend(['TriggerProducer:TriggerPass'])
-VectorInt.extend(['TriggerProducer:TriggerPrescales'])
-VectorString.extend(['TriggerProducer:TriggerNames'])
+process.TreeMaker2.VectorBool.extend(['TriggerProducer:TriggerPass'])
+process.TreeMaker2.VectorInt.extend(['TriggerProducer:TriggerPrescales'])
+process.TreeMaker2.VectorString.extend(['TriggerProducer:TriggerNames'])
 
 ###  - - - - - - - - Path stuff - - - - - - - 
 
@@ -118,3 +119,6 @@ process.Path = cms.Path(
                         
                         process.TreeMaker2
                         ) 
+
+
+
